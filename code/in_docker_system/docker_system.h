b12d8479f05c624d1add143dc41a29954947fe27
cadu.goncalves <cadu.goncalves@gmail.com>
2023-03-22 22:27:39 -0300

    Fixed define

--- code/in_docker_system/docker_system.h
+++ code/in_docker_system/docker_system.h
@@ -16,8 +16,8 @@
  *  limitations under the License.
  */
 
-#ifndef FLB_IN_DE_H
-#define FLB_IN_DE_H
+#ifndef FLB_IN_DSYSTEM_H
+#define FLB_IN_DSYSTEM_H
 
 #include <msgpack.h>
 #include <fluent-bit/flb_input.h>
